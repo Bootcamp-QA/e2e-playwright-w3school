11b110a1487a617a49dbc96dc4cc4c515e50204f
Bootcamp QA <adorandoamar@gmail.com>
2024-11-27 20:10:18 +0100

    fix visual tests

--- tests/test_visual_homepage.py
+++ tests/test_visual_homepage.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import Page, expect
+import utils
 
 def test_visual_homepage(page: Page, assert_snapshot):
-    print("Given user visit homepage")
-    page.goto("https://w3schools.com/")
+    if not utils.is_mobile(page):
+        print("Given user visit homepage")
+        page.goto("https://w3schools.com/")
 
-    #comprueba que la captura de la pagina es igual a la mostrada
-    assert_snapshot(page.screenshot())
+        #comprueba que la captura de la pagina es igual a la mostrada
+        assert_snapshot(page.screenshot())
